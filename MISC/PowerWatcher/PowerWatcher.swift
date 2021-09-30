4ca0321d841cddbac231199ed6dd1dfeb2573234
Abel Gancsos <agancsos@users.noreply.github.com>
2021-09-29 22:08:56 -0400

    Create PowerWatcher.swift

---
+++ MISC/PowerWatcher/PowerWatcher.swift
@@ -0,0 +1,35 @@
+import Foundation
+import AppKit
+
+// https://newbedev.com/what-event-is-fired-when-mac-is-back-from-sleep
+class PowerWatcher {
+    
+    public init() {
+    }
+    
+    @objc func onWake(note: NSNotification) {
+        print("System woke up...");
+    }
+    
+    @objc func onSleep(note: NSNotification) {
+        print("System fell asleep...");
+    }   
+    
+    private func registerNotifications() {
+        NSWorkspace.shared.notificationCenter.addObserver(self, selector: #selector(onWake), name: NSWorkspace.didWakeNotification, object: nil);
+        NSWorkspace.shared.notificationCenter.addObserver(self, selector: #selector(onSleep), name: NSWorkspace.willSleepNotification, object: nil);
+    }
+
+    public func watch() {
+        self.registerNotifications();
+    }
+}
+
+let group   : DispatchGroup = DispatchGroup();
+let watcher : PowerWatcher = PowerWatcher();
+
+for i : Int in 0..<CommandLine.arguments.count {
+
+}
+watcher.watch();
+RunLoop.current.run();
