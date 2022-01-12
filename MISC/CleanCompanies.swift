a9926c721ead548af33ee5d26170032f11a82f1e
Abel Gancsos <agancsos@users.noreply.github.com>
2022-01-12 16:21:55 -0500

    Update CleanCompanies.swift

--- MISC/CleanCompanies.swift
+++ MISC/CleanCompanies.swift
@@ -1,7 +1,7 @@
 import Foundation
 import EventKit
 
-public class Reminders {
+public class ReminderService {
     private var store             : EKEventStore;
 
     public  init() {
@@ -11,12 +11,12 @@ public class Reminders {
         });
     }
 
-    public func getReminders() -> [EKCalendar] {
+    public func getLists() -> [EKCalendar] {
         return self.store.calendars(for: .reminder)
     }
 
-    public func getReminder(name: String) -> EKCalendar? {
-        for cal : EKCalendar in self.getReminders() {
+    public func getList(name: String) -> EKCalendar? {
+        for cal : EKCalendar in self.getLists() {
             if cal.title == name {
                 return cal;
             }
@@ -24,71 +24,101 @@ public class Reminders {
         return nil;
     }
 
-    public func removeReminder(cal: EKCalendar, entry: String) {
+    public func getReminders(cal: EKCalendar) -> [EKReminder]? {
         let semaphore = DispatchSemaphore(value: 0);
-        var reminder : EKReminder?;
+        var result : [EKReminder]? = [];
         self.store.fetchReminders(matching: self.store.predicateForReminders(in: [cal]), completion: { rsp in
-            if rsp == nil { semaphore.signal(); }
-            for r in rsp! {
-                if r.title! == entry {
-                    reminder = r;
-                    semaphore.signal();
-                }
-            }
-            semaphore.signal();
-        });
-        semaphore.wait();
-        if reminder != nil {
-            try? self.store.remove(reminder!, commit: true);
-        }
-    }
-
-    public func entryExists(cal: EKCalendar, entry: String) -> Bool {
-        let semaphore = DispatchSemaphore(value: 0);
-        var result = false;
-        self.store.fetchReminders(matching: self.store.predicateForReminders(in: [cal]), completion: { rsp in
-            if rsp == nil { semaphore.signal(); }
-            for r in rsp! {
-                if r.title == entry {
-                    result = true;
-                    semaphore.signal();
-                }
-            }
+            result = rsp;
             semaphore.signal();
         });
         semaphore.wait();
         return result;
     }
+
+	public func getReminder(cal: EKCalendar, name: String) -> EKReminder? {
+		let semaphore = DispatchSemaphore(value: 0);
+        var result : EKReminder? = nil;
+        self.store.fetchReminders(matching: self.store.predicateForReminders(in: [cal]), completion: { rsp in
+			if rsp == nil { return; }
+			for temp in rsp! {
+				if temp.title! == name {
+            		result = temp;
+            		semaphore.signal();
+					break;
+				}
+			}
+			semaphore.signal();
+        });
+        semaphore.wait();
+        return result;
+    }
+
+    public func markCompleted(reminder: EKReminder) {
+        reminder.isCompleted = true;
+        try? self.store.save(reminder, commit: true);
+    }
+
+	public func removeReminder(reminder: EKReminder) {
+		try? self.store.remove(reminder, commit: true);
+	}
+
+	public func entryExists(cal: EKCalendar, entry: String) -> Bool {
+		return self.getReminder(cal: cal, name: entry) == nil;
+	}
 }
 
-var fileName = "";
-var listName = "";
-var isDebug  = false;
+var fileName       = "";
+var listName       = "";
+var isDebug        = false;
+var skipDuplicates = false;
 for i : Int in 0..<CommandLine.arguments.count {
-    if CommandLine.arguments[i] == "-f" { fileName = CommandLine.arguments[i + 1]; }
-    else if CommandLine.arguments[i] == "-l" { listName = CommandLine.arguments[i + 1]; }
-    else if CommandLine.arguments[i] == "--debug" { isDebug = true; }
+	if CommandLine.arguments[i] == "-f" { fileName = CommandLine.arguments[i + 1]; }
+	else if CommandLine.arguments[i] == "-l" { listName = CommandLine.arguments[i + 1]; }
+	else if CommandLine.arguments[i] == "--debug" { isDebug = true; }
+	else if CommandLine.arguments[i] == "--skip" { skipDuplicates = true; }
 }
-let service = Reminders();
+let service = ReminderService();
 if (listName == "") {
-    service.getReminders().forEach({ print($0.title); });
+	service.getLists().forEach({ print($0.title); });
 }
 else {
-    if fileName == "" {
-        print("Filename cannot be empty...");
-        exit(-1);
-    }
+	if fileName == "" && !skipDuplicates {
+		print("Filename cannot be empty...");
+		exit(-1);
+	}
 
-    let rawCompanies = try? String(contentsOf: URL(string: "file://\(fileName)")!, encoding: .utf8).split(separator: "\n");
-    let reminders = service.getReminder(name: listName);
-    if reminders != nil && rawCompanies != nil {
-        for company in rawCompanies! {
-            if service.entryExists(cal: reminders!, entry: String(company)) {
-                print("Cleaning: '\(company)'");
-                if !isDebug {
-                    service.removeReminder(cal: reminders!, entry: String(company));
-                }
+	let rawCompanies : [String] = [];
+	if !skipDuplicates {
+		try? String(contentsOf: URL(string: "file://\(fileName)")!, encoding: .utf8).split(separator: "\n");
+	}
+	let reminders = service.getList(name: listName);
+	if reminders != nil {
+		if !skipDuplicates {
+			for company in rawCompanies {
+				if service.entryExists(cal: reminders!, entry: String(company)) {
+					print("Cleaning: '\(company)'");
+					if !isDebug {
+						service.removeReminder(reminder: service.getReminder(cal: reminders!, name: String(company))!);
+					}
+					if (String(company).lowercased().contains(" gmbh") || String(company).lowercased().contains(" uk")) {
+                    	print("Company (\(String(company))) is not in the US");
+                    	if !isDebug {
+                        	service.markCompleted(reminder: service.getReminder(cal: reminders!, name: String(company))!);
+                    	}
+                	}
+				}
+			}
+		}
+		else {
+			for company in service.getReminders(cal: reminders!)!{
+            	if (company.title!.lowercased().contains(" gmbh") || company.title!.lowercased().contains(" uk")) {
+                	print("Company (\(company.title!)) is not in the US");
+                	if !isDebug {
+                    	service.markCompleted(reminder: company);
+                	}
+            	}
             }
-        }
-    }
+		}	
+	}
 }
+
